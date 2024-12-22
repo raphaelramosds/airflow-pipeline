297dfd7fc4b14d8c118c635ea64a0fb8e97d8708
Raphael Ramos <raphael.opensource@gmail.com>
2024-12-22 13:50:36 -0300

    first dag

---
+++ dags/hello_world.py
@@ -0,0 +1,18 @@
+"""
+hello world
+"""
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+with DAG (
+    dag_id = "hello_world",
+    start_date = datetime(2024, 12, 22),
+    schedule="@daily",
+):
+    start = EmptyOperator(task_id="start")
+    hello = BashOperator(task_id="hello", bash_command="echo hello world")
+    end = EmptyOperator(task_id="end")
+
+(start >> hello >> end)
