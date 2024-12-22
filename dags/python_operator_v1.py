4ace9dd2861a35a1d66730e8ef3916fe0ed3bedc
Raphael Ramos <raphael.opensource@gmail.com>
2024-12-22 14:30:58 -0300

    defining task as python operator

---
+++ dags/python_operator_v1.py
@@ -0,0 +1,29 @@
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
+import pendulum
+import pprint
+
+"""
+Task que imprime um nome estatico utilizando pprint
+
+Observação:
+ - BashOperator define task como um comando do bash
+ - PythonOperator define a task como uma função do python
+"""
+def meu_codigo(**kwargs):
+    print("andre")
+    pprint.pprint(kwargs)
+    return 123
+
+with DAG (
+    dag_id="python_operator_v1",
+    schedule="* * * * *", # Executar a cada minuto (notação do cron tab)
+    start_date=pendulum.datetime(2024,12,22,14,10,tz="America/Recife"),
+    catchup=True
+) as dag:
+    start = EmptyOperator(task_id="start")
+    python = PythonOperator(task_id="python", python_callable=meu_codigo)
+    end = EmptyOperator(task_id="end")
+
+start >> python >> end
---
+++ dags/python_operator_v2.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.decorators import task, dag
+import pendulum
+import pprint
+
+@dag (
+    schedule="* * * * *", # Executar a cada minuto (notação do cron tab)
+    start_date=pendulum.datetime(2024,12,22,14,26,tz="America/Recife"),
+    catchup=True
+)
+
+def python_operator_v2():
+    start = EmptyOperator(task_id="start")
+    end = EmptyOperator(task_id="end")
+
+    @task(task_id="python")
+    def meu_codigo(ds=None, **kwargs):
+        pprint.pprint(kwargs)
+        print(ds)
+        return "meu retorno agora é esse texto"    
+
+    python = meu_codigo()
+
+    start >> python >> end
+
+create_dag = python_operator_v2()
